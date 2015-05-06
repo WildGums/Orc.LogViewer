21bae79e37f4b7d9835b7fad6f8f6c59fa1fd6ef
Geert van Horrik <geert@catenalogic.com>
2015-05-06 13:09:58 +0200

    Created initial solution and controls

---
+++ src/Orc.LogViewer.Examples/Views/LogViewer.xaml.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogViewer.xaml.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.LogViewer.Examples.Views
+{
+    /// <summary>
+    /// Interaction logic for LogViewer.xaml.
+    /// </summary>
+    public partial class LogViewer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogViewer"/> class.
+        /// </summary>
+        public LogViewer()
+        {
+            InitializeComponent();
+        }
+
+        //private void LogViewerControlOnLogRecordDoubleClick(object sender, LogEntryDoubleClickEventArgs e)
+        //{
+        //    EventsTextBox.AppendText(e.LogEntry.Message + " clicked" + Environment.NewLine);
+        //}
+    }
+}
---
+++ src/Orc.LogViewer/Orc.LogViewer.Shared/ModuleInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Catel.IoC;
+using Catel.MVVM;
+using Orc.LogViewer;
+using Orc.LogViewer.ViewModels;
+
+/// <summary>
+/// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
+/// </summary>
+public static class ModuleInitializer
+{
+    /// <summary>
+    /// Initializes the module.
+    /// </summary>
+    public static void Initialize()
+    {
+        var serviceLocator = ServiceLocator.Default;
+
+        var viewModelLocator = serviceLocator.ResolveType<IViewModelLocator>();
+        //viewModelLocator.Register(typeof(DateTimePickerControl), typeof(DateTimePickerViewModel));
+        //viewModelLocator.Register(typeof(TimeSpanControl), typeof(TimeSpanViewModel));
+        //viewModelLocator.Register(typeof(DropDownButton), typeof(DropDownButtonViewModel));
+    }
+}
