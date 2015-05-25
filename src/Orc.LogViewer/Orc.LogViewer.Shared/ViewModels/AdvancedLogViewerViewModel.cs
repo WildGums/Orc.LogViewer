a770d774f04140151b7542df4d542d020efcc73a
Tanya Pristupova <zyrk@zyrk.net>
2015-05-25 16:18:21 +0100

    ORCOMP-112 Fixes red border around checked toggle buttons.

--- src/Orc.LogViewer/Orc.LogViewer.Shared/ViewModels/AdvancedLogViewerViewModel.cs
+++ src/Orc.LogViewer/Orc.LogViewer.Shared/ViewModels/AdvancedLogViewerViewModel.cs
@@ -15,10 +15,12 @@ namespace Orc.LogViewer.ViewModels
     public class AdvancedLogViewerViewModel : ViewModelBase
     {
         private Brush _accentColorBrushProperty;
+        private LogEvent _level;
 
         #region Constructors
         public AdvancedLogViewerViewModel()
         {
+            _level = LogEvent.Error | LogEvent.Warning | LogEvent.Info;
         }
         #endregion
 
@@ -31,7 +33,96 @@ namespace Orc.LogViewer.ViewModels
 
         public bool ShowFilterBox { get; set; }
 
-        public LogEvent Level { get; set; }
+        public LogEvent Level
+        {
+            get { return _level; }
+            set
+            {
+                if (_level != value)
+                {
+                    RaisePropertyChanged("ErrorChecked");
+                    RaisePropertyChanged("WarningChecked");
+                    RaisePropertyChanged("DebugChecked");
+                    RaisePropertyChanged("InfoChecked");
+                };
+                _level = value;
+            }
+        }
+
+        public bool ErrorChecked
+        {
+            get { return Level.HasFlag(LogEvent.Error); }
+            set
+            {
+                if (value)
+                {
+                    Level |= LogEvent.Error;
+                }
+                else
+                {
+                    Level &= ~LogEvent.Error;
+                }
+                RaisePropertyChanged("Level");
+                RaisePropertyChanged("ErrorChecked");
+            }
+        }
+
+        public bool WarningChecked
+        {
+            get { return Level.HasFlag(LogEvent.Warning); }
+            set
+            {
+                if (value)
+                {
+                    Level |= LogEvent.Warning;
+                }
+                else
+                {
+                    Level &= ~LogEvent.Warning;
+                }
+                RaisePropertyChanged("Level");
+                RaisePropertyChanged("WarningChecked");
+
+            }
+        }
+
+        public bool InfoChecked
+        {
+            get { return Level.HasFlag(LogEvent.Info); }
+            set
+            {
+                if (value)
+                {
+                    Level |= LogEvent.Info;
+                }
+                else
+                {
+                    Level &= ~LogEvent.Info;
+                }
+                RaisePropertyChanged("Level");
+                RaisePropertyChanged("InfoChecked");
+
+            }
+        }
+
+        public bool DebugChecked
+        {
+            get { return Level.HasFlag(LogEvent.Debug); }
+            set
+            {
+                if (value)
+                {
+                    Level |= LogEvent.Debug;
+                }
+                else
+                {
+                    Level &= ~LogEvent.Debug;
+                }
+                RaisePropertyChanged("Level");
+                RaisePropertyChanged("DebugChecked");
+
+            }
+        }
 
         public Brush AccentColorBrush
         {
