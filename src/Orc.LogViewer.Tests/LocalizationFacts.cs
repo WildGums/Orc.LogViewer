6b916fc30350429f4741df60cb3ff215d38bc561
Geert van Horrik <geert@catenalogic.com>
2026-05-16 21:44:39 +0200

    Update

--- src/Orc.LogViewer.Tests/LocalizationFacts.cs
+++ src/Orc.LogViewer.Tests/LocalizationFacts.cs
@@ -18,22 +18,6 @@ public class LocalizationFacts
 
             Assert.That(languageService.GetRequiredString("LogViewer_AdvancedLogViewerControl_Button_Content_EditFilterGroups"), Is.EqualTo("..."));
         }
-
-        [Test]
-        public void Return_Expected_Example_Strings()
-        {
-            using var serviceProvider = CreateServiceProvider();
-            var languageService = serviceProvider.GetRequiredService<ILanguageService>();
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(languageService.GetRequiredString("LogViewerExample_MainWindow_TabItem_Header_LogViewer"), Is.EqualTo("LogViewer"));
-                Assert.That(languageService.GetRequiredString("LogViewerExample_MainWindow_TabItem_Header_LogViewerWindow"), Is.EqualTo("LogViewer window"));
-                Assert.That(languageService.GetRequiredString("LogViewerExample_LogViewerSeparateWindowExample_Button_Content_OpenLogViewerInSeparateWindow"), Is.EqualTo("Open log viewer in separate window"));
-                Assert.That(languageService.GetRequiredString("LogViewerExample_MainWindow_Title"), Is.EqualTo("Orc.LogViewer example"));
-                Assert.That(languageService.GetRequiredString("LogViewerExample_LogWindow_Title"), Is.EqualTo("Log window"));
-            });
-        }
     }
 
     private static ServiceProvider CreateServiceProvider()
